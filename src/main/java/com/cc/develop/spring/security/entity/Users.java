b0a13009c3612b8418f2d79af974d6cafb9ca27d
cc <ziyoushigv@163.com>
2020-11-10 10:45:26 +0800

    添加用户实体类、以及对应的mapper

---
+++ src/main/java/com/cc/develop/spring/security/entity/Users.java
@@ -0,0 +1,19 @@
+package com.cc.develop.spring.security.entity;
+
+import lombok.Data;
+
+/**
+ * com.cc.develop.spring.security.entity
+ *
+ * @author changchen
+ * @email java@mail.com
+ * @date 2020-11-10 10:28:08
+ */
+@Data
+public class Users {
+
+    private Integer id;
+    private String username;
+    private String password;
+
+}
---
+++ src/main/java/com/cc/develop/spring/security/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.cc.develop.spring.security.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cc.develop.spring.security.entity.Users;
+import org.springframework.stereotype.Repository;
+
+/**
+ * com.cc.develop.spring.security.mapper
+ *
+ * @author changchen
+ * @email java@mail.com
+ * @date 2020-11-10 10:29:22
+ */
+@Repository
+public interface UserMapper extends BaseMapper<Users> {
+
+}
