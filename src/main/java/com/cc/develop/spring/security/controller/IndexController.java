3e2f59f3a88b048d76f158981847f83d2b32ba11
cc <ziyoushigv@163.com>
2020-11-12 20:43:45 +0800

    方法上添加@Secured注解

--- src/main/java/com/cc/develop/spring/security/controller/IndexController.java
+++ src/main/java/com/cc/develop/spring/security/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.cc.develop.spring.security.controller;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,12 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
  * @email java@mail.com
  * @date 2020-11-10 08:50:21
  */
+@RequestMapping("/test")
 @RestController
 public class IndexController {
 
     @GetMapping("/index")
     public String index(){
         return "success";
+    }
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello spring security";
+    }
+
+    @Secured({"ROLE_role","ROLE_admin"})
+    @GetMapping("/update")
+    public String update(){
+        return "hello update";
     }
 
 }
