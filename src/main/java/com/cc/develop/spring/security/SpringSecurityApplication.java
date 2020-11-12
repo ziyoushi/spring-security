2af51a04eee2fa5b6f1b3835656f8a29496f1990
cc <ziyoushigv@163.com>
2020-11-12 20:41:11 +0800

    在启动类上开启 方法注解生效

--- src/main/java/com/cc/develop/spring/security/SpringSecurityApplication.java
+++ src/main/java/com/cc/develop/spring/security/SpringSecurityApplication.java
@@ -1,8 +1,13 @@
 package com.cc.develop.spring.security;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+// 启动类上开启注解功能
+@EnableGlobalMethodSecurity(securedEnabled = true)
+@MapperScan(basePackages = "com.cc.develop.spring.security.mapper")
 @SpringBootApplication
 public class SpringSecurityApplication {
 
